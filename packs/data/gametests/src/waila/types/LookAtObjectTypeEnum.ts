ef1703756f5963a2ef4f7c2f6ec241101581461b
liuaotou <liuaotou@gmail.com>
2026-08-10 13:14:34 +0800

    Add packs/data/gametests/src/waila/types/LookAtObjectTypeEnum.ts

---
+++ packs/data/gametests/src/waila/types/LookAtObjectTypeEnum.ts
@@ -0,0 +1,24 @@
+/**
+ *
+ * @author
+ * r4isen1920
+ * https://mcpedl.com/user/r4isen1920
+ *
+ * @license
+ * MIT License
+ *
+ */
+
+/**
+ * List of all the possible look at object types.
+ */
+export enum LookAtObjectTypeEnum {
+	/**
+	 * A moving entity, such as a player or a mob.
+	 */
+	ENTITY = "entity",
+	/**
+	 * A static block or tile in the world.
+	 */
+	TILE = "tile",
+}
