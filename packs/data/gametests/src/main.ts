d4f62ba6d952c1f63a0c8cbfe40cf64a4c099ffa
liuaotou <liuaotou@gmail.com>
2026-08-10 13:13:39 +0800

    Add packs/data/gametests/src/main.ts

---
+++ packs/data/gametests/src/main.ts
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author
+ * r4isen1920
+ * https://mcpedl.com/user/r4isen1920
+ *
+ * @license
+ * MIT License
+ *
+ */
+
+import "./waila/utils/Logger";
+import "./waila/utils/String";
+import "./waila/utils/Version";
+import "./waila/core/Settings";
+import "./waila/core/Waila";
+
+import { init } from "@bedrock-oss/stylish";
+init();
